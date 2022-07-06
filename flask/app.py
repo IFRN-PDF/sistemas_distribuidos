75f70f428c909cdf995131a95d96390c336d7b1d
Demetrios Coutinho <demetriosamc@gmail.com>
2022-07-06 15:20:35 -0300

    exemplo de banco de dados

--- flask/app.py
+++ flask/app.py
@@ -1,28 +1,28 @@
 from flask import Flask, request, jsonify
-
+import db
+    
 app = Flask(__name__)
-
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-]
-
-def _find_next_id():
-    return max(country["id"] for country in countries) + 1
 
 @app.route("/countries",methods=['GET'])
 def get_countries():
-    return jsonify(countries)
+    countries = db.query_db('select * from countries')
+    return jsonify(countries),200
 
 @app.route("/countries",methods=['POST'])
 def add_country():
     if request.is_json:
         country = request.get_json()
-        country["id"] = _find_next_id()
-        countries.append(country)
-        return country, 201
+        id = db.insert((country['country_name'],country['capital']))
+        return {"id":id}, 201
     return {"error": "Request must be JSON"}, 415
 
 if __name__ == '__main__':
+    init_db = False
+    
+    db.init_app(app)
+    
+    if init_db:
+        with app.app_context():
+            db.init_db()
+    
     app.run(debug=True,host="0.0.0.0", port=8090)
---
+++ flask/db.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+DATABASE = 'database.db'
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+    
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            DATABASE,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def insert(args=()):
+    sql = ''' INSERT INTO countries(country_name,capital)
+              VALUES(?,?) '''
+    cur = get_db().cursor()
+    cur.execute(sql, args)
+    get_db().commit()
+    return cur.lastrowid
+    
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = [dict((cur.description[i][0], value) \
+       for i, value in enumerate(row)) for row in cur.fetchall()]
+   
+    get_db().commit()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+        
+def init_db():
+    db = get_db()
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
