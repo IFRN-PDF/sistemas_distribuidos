f74ba18daa9d95e16a2ed49ac88e0ce185ed5eae
Demetrios Coutinho <demetriosamc@gmail.com>
2023-04-20 13:31:49 -0300

    threads

--- threads/thread_ex2.py
+++ threads/thread_ex2.py
@@ -1,18 +1,22 @@
 import threading
 
 
-def soma():
+def soma(i):
     soma = 0
-    for i in range(20):
+    for i in range(20*i):
         soma += 1
     print(threading.current_thread().getName(), 'soma ', soma)
 
 lista_threads = list()
 
 for t in range(5):
-    th = threading.Thread(target=soma,name='Threading'+str(t))
+    th = threading.Thread(target=soma,args=(t+1000000,),name='Threading'+str(t))
     lista_threads.append(th)
     th.start()
 
-for thread in lista_threads:
-    thread.join()
+print("MEIO")
+
+#for thread in lista_threads:
+#    thread.join()
+    
+print("FINAL")
